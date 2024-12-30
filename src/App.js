a6985e111c04d8713d1324dea0e88f7195009e5f
sliver14 <silverchristopher12@gmail.com>
2024-12-30 14:57:53 +0100

    backend deployed finally

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './page2/Register';
 import Signin from './page2/Signin';
 import SummitFlyer from './pages/SummitFlyer';
-// import RegistrationPage from './pages/RegistrationPage';
+import RegistrationPage from './pages/RegistrationPage';
 import PrincipalsDashboard from './page2/PrincipalsDashboard';
 import Admin from './page2/Admin';
 import Ccss from "./pages/Ccss";
@@ -19,6 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<SummitFlyer />} />
         {/* <Route path="/create-flyer" element={<SummitFlyer />} /> */}
+        <Route path="/registrationpage" element={<RegistrationPage />} />
         <Route path="/principalsdashboard" element={<PrivateRoute><PrincipalsDashboard /></PrivateRoute>} />
         <Route path="/register" element={<Register />} />
         <Route path="/signin" element={<Signin />} />
--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -40,7 +40,7 @@ function RegistrationPage() {
     setSuccess("");
 
     try {
-      const response = await axios.post("http://localhost:3001/registration", e);
+      const response = await axios.post("my-summit-production.up.railway.app", e);
       setSuccess(response.data.message);
       navigate("/create-flyer");
     } catch(err) {
